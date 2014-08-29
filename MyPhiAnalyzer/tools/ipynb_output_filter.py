3ec0e646dc210d043661d7646c4ab183e46ab2da
Michael Eliachevitch <m.eliachevitch@gmx.net>
2014-08-29 14:22:03 +0200

    edited ipynb_output_filter.py comments
    explained that it doesn't work on lxplus, but can be run locally

--- MyPhiAnalyzer/tools/ipynb_output_filter.py
+++ MyPhiAnalyzer/tools/ipynb_output_filter.py
@@ -14,5 +14,9 @@ for sheet in json_in.worksheets:
 
 write(json_in, sys.stdout, 'json')
 
-#git config filter.dropoutput_ipynb.clean ~/bin/ipynb_output_filter.py
+# does not work yet on lxplus, because IPython.nbformat.current does not exist.
+# if you want to filter output from ipython notebook using this file,
+# work on the repository locally on your machine and run the following git commands there:
+
+#git config filter.dropoutput_ipynb.clean /path_to_filter/ipynb_output_filter.py
 #git config filter.dropoutput_ipynb.smudge cat
